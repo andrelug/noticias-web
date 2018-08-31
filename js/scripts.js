e596374a7db03663cb1da0132d960ef03976ac9e
Andre <andre@andrelug.com>
2018-08-31 11:10:47 -0300

    alterações para outbrain

--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,24 @@ function initCouponCounter(selector) {
       clearInterval(window[selector]);
     }
   }, 1000 * 60);
+
+  function dataAtualFormatada() {
+    var data = new Date();
+    var dia = data.getDate();
+    if (dia.toString().length == 1)
+      dia = "0" + dia;
+    var mes = data.getMonth() + 1;
+    if (mes.toString().length == 1)
+      mes = "0" + mes;
+    var ano = data.getFullYear();
+    return dia + "/" + mes + "/" + ano;
+  }
+
+  var hoje = document.getElementById('hoje');
+
+  hoje.innerText = dataAtualFormatada();
+
+  var atualizacaoHoje = document.getElementById('atualizacaoHoje');
+
+  atualizacaoHoje.innerText = dataAtualFormatada();
 }
